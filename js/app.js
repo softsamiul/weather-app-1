11ba2c9ad8ed7daa3c3bbfc44e412a4ea58db696
softsamiul <softsamiul@gmail.com>
2021-08-29 05:38:32 +0530

    Weather app almost complete

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', () => {
+    // geting search city
+    const getSearchInputTag = document.getElementById('location-input');
+    const searchLocation = getSearchInputTag.value;
+    console.log(searchLocation);
+
+    fetchApi(searchLocation);
+})
+
+// Fetching api
+const fetchApi = (searchLocation) => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=c35d689fc18f475e83c231327212808&q=${searchLocation}&aqi=yes`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayData(data))
+}
+
+// Display fetched data
+const displayData = (data) => {
+    console.log(data);
+    // Update location
+    const cityName = document.getElementById('city');
+    cityName.innerText = data.location.name;
+    // update temprature
+    const temp = document.getElementById('temp');
+    temp.innerText = data.current.temp_c;
+
+    // Update weather-condition-icon
+    const tempIcon = document.getElementById('weather-condition-icon');
+    tempIcon.src = data.current.condition.icon;
+
+}
